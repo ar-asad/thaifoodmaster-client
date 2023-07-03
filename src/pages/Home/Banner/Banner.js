2d03485df6572f3ab30e4ca109d9086a1a6c3760
arasad1 <asaduzzamanaj07@gmail.com>
2023-07-03 17:56:46 +0600

    banner image changed and navbar style changed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import hero from '../../../assets/images/hero/hero.png'
+import hero from '../../../assets/images/hero/hero.jpg'
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen bg-orange-100">
-            <div className="hero-content flex-col lg:flex-row-reverse gap-20">
-                <img src={hero} alt='' className="lg:w-1/2 rounded-lg shadow-" />
-                <div>
-                    <h1 className="text-6xl font-bold">The healthiest way to start your day</h1>
-                    <p className="py-6 text-xl font-semibold mb-5">The smart 365-days-per-year food subscription that will make you eat healthy again. Tailored to your personal tastes and nutritional needs.</p>
-                    <button className="btn bg-blue-500 border-0">Get Started</button>
+        // <div className="hero min-h-screen" style={{ backgroundImage: `url(${hero})` }}>
+        //     <div className="hero-overlay bg-opacity-30"></div>
+        //     <div className=" flex text-neutral-content">
+        //         <div className="max-w-full px-3">
+        //             <div className='w-1/2'>
+        //                 <p className='uppercase text-green-500 font-bold tracking-widest text-xl mb-5 '>Welcome to AutoMotive !</p>
+        //                 <h1 className="mb-5 lg:text-6xl text-4xl font-bold">Incridible and delicious healthy thai food</h1>
+        //                 <p className="mb-8 text-lg">Cartime is a prestige used car supermarket specialising in quality toy cars. We offer a friendly and efficient service in buying and selling.</p>
+        //                 <button className="btn border-0 btn-success mr-6 mb-2 text-gray-100 bg-green-500 rounded">Get Started &rarr;</button>
+        //                 <button className="btn border-0 bg-gray-100 text-green-500 rounded">About More &rarr;</button>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${hero})` }}>
+            <div className="hero-overlay bg-opacity-30"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-lg">
+                    <h1 className="mb-5 text-5xl font-bold">Incridible and delicious healthy thai food</h1>
+                    <p className="mb-5">Thai cuisine is known for its vibrant flavors, aromatic herbs, and balanced combination of sweet, sour, salty, and spicy elements</p>
+                    <button className="btn bg-blue-500 border-0">Read More</button>
                 </div>
             </div>
         </div>
--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AuthContex } from '../../context/AuthProvider/AuthProvider';
 import LazyLoad from 'react-lazy-load';
+
+const style = {
+    fontFamily: 'Kalam, cursive'
+};
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContex);
@@ -13,48 +17,72 @@ const Navbar = () => {
 
     const menuItem = <>
 
-        <li><NavLink to='/' className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""}>Home</NavLink></li>
-        <li><NavLink to='/blog' className={({ isActive, isPending }) =>
-            isPending ? "pending" : isActive ? "active" : ""}>Blog</NavLink></li>
+        <li><Link to='/' className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""}>Home</Link></li>
+        <li><Link to='/blog' className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""}>Blog</Link></li>
         {
             user?.uid ?
                 <div className='flex items-center'>
-                    <li className='font-semibold '>
+
+                    <button className="btn btn-ghost lg:text-lg" onClick={handleLogOut}>Log out</button>
+                </div>
+                :
+                <li><Link to='/login' className={({ isActive, isPending }) =>
+                    isPending ? "pending" : isActive ? "active" : ""}>Login</Link></li>
+        }
+    </>
+    return (
+        // <div className="navbar fixed z-10 top-0 bg-transparent" >
+        //     <div className="navbar-center" >
+        //         <div className="dropdown" >
+        //             <label tabIndex="0" className="btn btn-ghost lg:hidden" >
+        //                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg >
+        //             </label >
+        //             <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3  shadow bg-orange-200 rounded-box w-52 font-semibold " >
+        //                 {menuItem}
+        //             </ul >
+        //         </div >
+        //         {/* <Link className="btn btn-ghost normal-case text-3xl text-blue-500 "><span className='text-orange-500'>Thai </span>Foodmaster</Link > */}
+        //         <div className=" hidden lg:flex" >
+        //             <ul className="menu menu-horizontal p-0 font-semibold text-xl mr-8 flex items-center" >
+        //                 {menuItem}
+        //             </ul >
+        //         </div >
+        //     </div >
+
+        // </div >
+        <div className="navbar bg-black text-white opacity-90 ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItem}
+                    </ul>
+                </div>
+                <Link style={style} className="btn btn-ghost normal-case text-xl text-blue-500 "><span className='text-orange-500'>Thai </span>Foodmaster</Link >
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {menuItem}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {
+                    user?.uid && <li className='font-semibold list-none mr-4'>
                         <div title={user?.displayName} className="avatar">
-                            <div className="w-16 h-16 rounded-full">
+                            <div className="w-12 h-12 rounded-full">
                                 <LazyLoad>
                                     <img src={user?.photoURL} alt='' />
                                 </LazyLoad>
                             </div>
                         </div>
                     </li>
-                    <button className="btn btn-ghost lg:text-lg" onClick={handleLogOut}>Log out</button>
-                </div>
-                :
-                <li><NavLink to='/login' className={({ isActive, isPending }) =>
-                    isPending ? "pending" : isActive ? "active" : ""}>Login</NavLink></li>
-        }
-    </>
-    return (
-        <div className="navbar bg-orange-100" >
-            <div className="navbar-start" >
-                <div className="dropdown" >
-                    <label tabIndex="0" className="btn btn-ghost lg:hidden" >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg >
-                    </label >
-                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-orange-200 rounded-box w-52 font-semibold text-xl" >
-                        {menuItem}
-                    </ul >
-                </div >
-                <Link className="btn btn-ghost normal-case text-3xl text-blue-500 "><span className='text-orange-500'>Thai </span>Foodmaster</Link >
-            </div >
-            <div className="navbar-end hidden lg:flex" >
-                <ul className="menu menu-horizontal p-0 font-semibold text-xl mr-8" >
-                    {menuItem}
-                </ul >
-            </div >
-        </div >
+                }
+            </div>
+        </div>
     );
 };
 
